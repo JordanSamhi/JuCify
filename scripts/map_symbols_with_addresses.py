a1693c7a1ebe01ccd6f1230ddc67c64d8f3d8356
Jordan Samhi <j.samhi@me.com>
2021-07-28 15:23:04 +0200

    Add: end address

--- scripts/map_symbols_with_addresses.py
+++ scripts/map_symbols_with_addresses.py
@@ -8,4 +8,4 @@ json_file = open(f)
 json_data = json.load(json_file)
 
 for elt in json_data["functions"]:
-    print(f"{elt['name']}:{elt['startAddr']}")
+    print(f"{elt['name']}:{elt['startAddr']}:{elt['endAddr']}")
