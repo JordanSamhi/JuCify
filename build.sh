7daf4d6569186e013479ecd568efdacd31369580
Jordan Samhi <j.samhi@me.com>
2021-08-04 14:12:40 +0200

    Add: libs

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
 export MAVEN_OPTS=-Xss32m
+mvn clean install:install-file -Dfile=libs/soot-infoflow-android-classes.jar -DgroupId=de.tud.sse -DartifactId=soot-infoflow-android -Dversion=2.7.1 -Dpackaging=jar
+mvn clean install:install-file -Dfile=libs/soot-infoflow-classes.jar -DgroupId=de.tud.sse -DartifactId=soot-infoflow -Dversion=2.7.1 -Dpackaging=jar
 mvn clean install
